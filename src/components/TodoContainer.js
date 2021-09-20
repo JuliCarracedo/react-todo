c63096bea8af7279131d2141f74cadc836ba0d1f
JuliCarracedo <julicarra@hotmail.com>
2021-09-20 20:32:16 -0300

    Add up to routing

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,28 +1,16 @@
-import React, { useState } from "react"
-import InputTodo from "../InputTodo";
-import TodoList from "../TodoList";
+import React, { useEffect, useState } from "react"
+import InputTodo from "./InputTodo";
+import TodoList from "./TodoList";
 import Header from "./Header"
+import About from "../pages/About"
+import NotMatch from "../pages/NotMatch"
+import { Route, Switch } from "react-router-dom"
+import Navbar from "./Navbar"
 
 const TodoContainer = () => {
-    const [todos, setTodos] = useState(
-        [
-          {
-            id: 1,
-            title: "Setup development environment",
-            completed: true
-          },
-          {
-            id: 2,
-            title: "Develop website and add content",
-            completed: false
-          },
-          {
-            id: 3,
-            title: "Deploy to live server",
-            completed: false
-          }
-        ]
-       );
+    const [todos, setTodos] =  useState(
+      JSON.parse(localStorage.getItem('list')) || []
+    );
     
     const addTodoItem = title => {
         const newTodo = {
@@ -33,6 +21,7 @@ const TodoContainer = () => {
           setTodos(
             [...todos, newTodo]
           );
+        localStorage.setItem('list', JSON.stringify(todos));
       };
 
     const handleChange = (id) => {
@@ -44,11 +33,16 @@ const TodoContainer = () => {
               return todo;
             })
           );
+
+          localStorage.setItem('list', JSON.stringify(todos));
     };
 
     const delTodo = (id) => {
         setTodos( todos.filter(todo => todo.id != id) );
+
+        localStorage.setItem('list', JSON.stringify(todos));
       }
+
     const setUpdate = (updatedTitle, id) => {
         setTodos(todos.map(todo => {
             if (todo.id === id) {
@@ -57,16 +51,37 @@ const TodoContainer = () => {
             return todo
           })
         );
+        localStorage.setItem('list', JSON.stringify(todos));
       }
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+        .then(response => response.json())
+        .then(data => setTodos(data));
+      });
+
     return (
-            <div className="container">
-                <div className="inner">
-                    <Header />
-                    <InputTodo addTodoProps={addTodoItem}/>
-                    <TodoList todos={todos} handleChangeProps={handleChange} 
-                    deleteTodoProps={delTodo}  setUpdate={setUpdate}/>
-                </div>
-            </div>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <div className="container">
+                  <div className="inner">
+                      <Header />
+                      <InputTodo addTodoProps={addTodoItem}/>
+                      <TodoList todos={todos} handleChangeProps={handleChange} 
+                      deleteTodoProps={delTodo}  setUpdate={setUpdate}/>
+                  </div>
+              </div>
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="*">
+              <NotMatch />
+            </Route>
+          </Switch>
+        </>
         );
 
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom"
 import TodoContainer from "./components/TodoContainer"
 import "./App.css"
+import { BrowserRouter } from "react-router-dom"
 
-ReactDOM.render(<TodoContainer />, document.getElementById("root"));
+ReactDOM.render(
+    (<React.StrictMode>
+        <BrowserRouter>
+            <TodoContainer />
+        </BrowserRouter>
+    </React.StrictMode>) , document.getElementById("root"));
